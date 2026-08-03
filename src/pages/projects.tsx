cf0a7fa6f1915a76c81f3e705bb0993823eefa6e
Suman Bhartiya <skb08725@gmail.com>
2026-08-03 19:08:41 +0530

    fix : projects page

---
+++ src/pages/projects.tsx
@@ -0,0 +1,14 @@
+import { SectionHeader } from '../components/ui'
+
+export default function ProjectPage() {
+    return (
+        <div className="min-h-screen">
+            <section>
+                <div className="max-w-7xl mx-auto px-4 py-2">
+                    <SectionHeader badge="Our Projects" title="We are coming  soon with our projects !!" />
+                </div>
+            </section>
+
+        </div>
+    )
+}
